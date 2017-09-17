e0f855ab32118df3295255827bb495175ca1d9b1
wjbmorgan <wjbmorgan@hotmail.com>
2017-09-17 21:00:41 +1000

    init

---
+++ Assets/MenuScript/BackMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.Advertisements;
+
+public class BackMenu: MonoBehaviour {
+	public void back(){
+		if (PlayerPrefs.GetInt("removeAds",0)==0){
+			ShowAd();
+		}
+		SceneManager.LoadScene ("Menu");
+	}
+
+
+
+	public void ShowAd()
+	{
+		if (Advertisement.IsReady ()) {
+			Advertisement.Show ();
+		} else {
+			Advertisement.Initialize ("1165124");
+		}
+	}
+}
---
+++ Assets/MenuScript/OpenAds.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class OpenAds : MonoBehaviour
+{
+	public void ShowAd()
+	{
+		if (Advertisement.IsReady())
+		{
+			Advertisement.Show();
+		}
+	}
+}
+
